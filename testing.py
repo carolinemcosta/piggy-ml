34f53711c48a1531634235eb7e9d5060956051f7
carolinemcosta <caluline@gmail.com>
2021-05-10 18:00:02 +0100

    added functions to split dataset into training and testing. Added some documentation

--- testing.py
+++ testing.py
@@ -1,14 +1,32 @@
 import os
 import pandas as pd
 import get_data as gd
+import matplotlib.pyplot as plt
+from training_set import split_train_test_pig
 
 def main():
-  # fetch data from Dropbox
+  # fetch data from Dropbox folder
   gd.fetch_pig_data()
   
   # load data to dataframe
   pig_data = gd.load_pig_data()
-  print(pig_data.head())
+  #print(pig_data.head())
+  
+  # plot the data
+  #data = pig_data[["AMP","DVDT","ARI"]]
+  #data.hist(bins=50,figsize=(15,15))
+  #plt.show()
+  
+  #pig = pig_data[pig_data.PIG.isin([20,21])]
+  #print(pig)
+  
+  #print(pig_data.PIG.unique())
+  
+  train_set, test_set = split_train_test_pig(pig_data, 0.2)
+  print(train_set.head())
+  print(test_set.head())
+  
+  print(len(train_set), len(test_set))
   
 if __name__== "__main__":
   main()
---
+++ training_set.py
@@ -0,0 +1,57 @@
+import numpy as np
+import math
+
+def split_train_test(data, test_ratio):
+  ''' Splits the data into traning and test sets using a random permutation.
+      
+      Parameters: 
+        data (Pandas DataFrame): Data prior to splitting into training and testing
+        test_ratio (float): the ratio to split the data into. Typically 20% with test_ratio = 0.2
+        
+      Returns:
+        Two Pandas DataFrames, one with the trainig set and one with testing set
+  '''
+  
+  # generate the random permutations
+  np.random.seed(42) # set a seed to always get the same split
+  shuffled_indices = np.random.permutation(len(data))
+  test_set_size = int(len(data)*test_ratio)
+  
+  # split the indices
+  test_indices = shuffled_indices[:test_set_size]
+  train_indices = shuffled_indices[test_set_size:]
+  
+  # return split data
+  return data.iloc[train_indices], data.iloc[test_indices]
+
+def split_train_test_pig(data, test_ratio):
+  ''' Splits the data into traning and test sets using a random permutation, 
+      but ensuring that at least one whole pig dataset is in the testing set.
+      
+      Parameters: 
+        data (Pandas DataFrame): Data prior to splitting into training and testing
+        test_ratio (float): the ratio to split the data into. Typically 20% with test_ratio = 0.2
+        
+      Returns:
+        Two Pandas DataFrames, one with the trainig set and one with testing set
+  '''
+  # number of unique pigs
+  pigs = data.PIG.unique()
+  total_pigs = len(pigs)
+  
+  # generate the random permutations
+  np.random.seed(42) # set a seed to always get the same split
+  shuffled_indices = np.random.permutation(total_pigs)
+  test_set_size = round(total_pigs*test_ratio) # round to nearest integer
+  
+  # split the indices
+  test_indices = shuffled_indices[:test_set_size]
+  train_indices = shuffled_indices[test_set_size:]
+  
+  # split the data
+  test_pigs = pigs[test_indices]
+  test_data = data[data.PIG.isin(test_pigs)]  
+  train_pigs = pigs[train_indices]
+  train_data = data[data.PIG.isin(train_pigs)]
+  
+  return train_data, test_data
