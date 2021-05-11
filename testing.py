e2a59da16da9c2eb3cdc94fed0613b7b3b77044b
carolinemcosta <caluline@gmail.com>
2021-05-11 10:20:39 +0100

    minor changes

--- testing.py
+++ testing.py
@@ -12,21 +12,16 @@ def main():
   pig_data = gd.load_pig_data()
   #print(pig_data.head())
   
-  # plot the data
-  #data = pig_data[["AMP","DVDT","ARI"]]
-  #data.hist(bins=50,figsize=(15,15))
-  #plt.show()
-  
-  #pig = pig_data[pig_data.PIG.isin([20,21])]
-  #print(pig)
-  
-  #print(pig_data.PIG.unique())
-  
   train_set, test_set = split_train_test_pig(pig_data, 0.2)
-  print(train_set.head())
-  print(test_set.head())
+  #print(train_set.head())
+  #print(test_set.head())
   
-  print(len(train_set), len(test_set))
+  #print(len(train_set), len(test_set))
+
+  # plot the data
+  data = train_set[["AMP","DVDT","ARI"]]
+  data.hist(bins=50,figsize=(15,15))
+  plt.show()
   
 if __name__== "__main__":
   main()
