11b85648407175d637ae8e3c86b47fe674b00c95
carolinemcosta <caluline@gmail.com>
2021-07-22 14:52:53 +0100

    printint score

--- evaluate_model.py
+++ evaluate_model.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import GridSearchCV
 
-def evaluate_classifier(clf, param_grid, scoring, refit, splits, data, labels, groups):
+def evaluate_classifier(clf, param_grid, scoring, splits, data, labels, groups, file_name):
   ''' Tune hyperparameters and evaluate model with cross validation
       
       Parameters: 
@@ -20,18 +20,15 @@ def evaluate_classifier(clf, param_grid, scoring, refit, splits, data, labels, g
         trained_clf: trained and evaluated classifier
   '''    
   # tune hyperparameters
-  tuned_clf = GridSearchCV(clf, param_grid, scoring=scoring, refit=refit, cv=splits, return_train_score=True)  
+  tuned_clf = GridSearchCV(clf, param_grid, scoring=scoring, cv=splits, return_train_score=True)  
   tuned_clf.fit(data, labels, groups=groups)
   
   print(tuned_clf.best_params_)
   # evaluate best estimator
   scores = cross_validate(tuned_clf.best_estimator_, data, labels, scoring=scoring, cv=splits, groups=groups, return_train_score=True)
 
-  # print scores
-  for scr in scoring:
-    print("%s:"%scr, scores["test_%s"%scr])
-    print("mean %s:"%scr, np.mean(scores["test_%s"%scr]))
-    
+  print(scores["train_score"])
+  print(np.mean(scores["train_score"]))
   
   return tuned_clf.best_estimator_
   
